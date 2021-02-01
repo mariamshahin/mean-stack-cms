777796374078f7ab3026902f9a496a989d00c713
Mariam Shahin <mariam.shahin@qowwa.net>
2021-02-01 08:09:37 +0200

    updated cors header

--- client/server.js
+++ client/server.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+  next();
+});
 app.use(express.static(__dirname + "/dist/client"));
-
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/client/index.html"));
 });
-
 app.listen(process.env.PORT || 8080);
