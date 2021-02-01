56825d73117034ac8b04f1a53b08f999692119b7
Mariam Shahin <mariam.shahin@qowwa.net>
2021-02-01 10:31:12 +0200

    removed prefix and rewrited the path in the client

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,10 +5,9 @@ import drafts from './drafts';
 import comments from './comments';
 
 export default (app) => {
-  const prefix = '/api';
-  app.use(prefix + '/auth', auth);
-  app.use(prefix + '/users', users);
-  app.use(prefix + '/posts', posts);
-  app.use(prefix + '/drafts', drafts);
-  app.use(prefix + '/comments', comments);
+  app.use('/auth', auth);
+  app.use('/users', users);
+  app.use('/posts', posts);
+  app.use('/drafts', drafts);
+  app.use('/comments', comments);
 };
